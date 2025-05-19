c8e147f097bfc984187e13c4754f242495e8facf
cooliny <colinyeung.main@gmail.com>
2025-05-19 02:31:10 -0700

    Update tim1637.h
    Code cleanup

--- tim1637.h
+++ tim1637.h
@@ -1,8 +1,9 @@
- #ifndef TIM1637_H
- #define TIM1637_H
+#ifndef TIM1637_H
+#define TIM1637_H
 
- #define SYSCLK 32000000L
+#define SYSCLK 32000000L
 
+void tm1637Init(void); 
 void _tm1637Start(void);
 void _tm1637Stop(void);
 void _tm1637ReadResult(void);
@@ -12,5 +13,10 @@ void _tm1637ClkHigh(void);
 void _tm1637ClkLow(void);
 void _tm1637DioHigh(void);
 void _tm1637DioLow(void);
+void tm1637SetBrightness(char brightness);
+void tm1637DisplayDecimal(int v, int displaySeparator); 
+void tm1637ScrollMessage(const char* message, int delay_ms); 
+char chartosegment(char c); 
+void checkReady(void);
 
 #endif /* TIM1637_H */
