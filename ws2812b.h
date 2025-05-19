e497a07de97aa06c6b00abfb357155a3197ea2e2
cooliny <colinyeung.main@gmail.com>
2025-05-19 16:18:57 -0700

    Update ws2812b.h
    Final cleanup

--- ws2812b.h
+++ ws2812b.h
@@ -4,12 +4,15 @@
 #define SYSCLK 32000000L
 #define NUM_LEDS 35
 
+#define WS2812B_PORT GPIOA
+#define WS2812B_PIN 7
+
 void spi_init(void);
 void spi_send(uint8_t data);
 void encode_byte(uint8_t in, uint8_t *out);
 void set_pixel(int led_index, uint8_t r, uint8_t g, uint8_t b);
 void send_buffer(int len);
-void ws2812_latch(void);
+void ws2812b_reset(void);
 void set_colour(uint8_t r, uint8_t g, uint8_t b);
 void spin_animation(int num_leds, uint8_t r, uint8_t g, uint8_t b, int active_leds, int delay); 
 void flash_animation(int num_leds, uint8_t r, uint8_t g, uint8_t b, int flashes, int delay);
