e9b470df7b4f9e26dcfe54bc129d51f0c2cec44e
cooliny <colinyeung.main@gmail.com>
2025-05-19 16:17:57 -0700

    Update util.h
    Final cleanup

--- util.h
+++ util.h
@@ -1,5 +1,5 @@
 /*
- * Coin Picking Robot (Remote)
+ * Hockey table
  * util.h
  */
 
@@ -7,19 +7,33 @@
  #define UTIL_H
  
  #define SYSCLK 32000000L
+
+// LCD Display
+#define LCD_E_PORT GPIOB
+#define LCD_E_PIN 3 
+#define LCD_RS_PORT GPIOA
+#define LCD_RS_PIN 15
+#define LCD_D4_PORT GPIOA
+#define LCD_D4_PIN 14 
+#define LCD_D5_PORT GPIOA
+#define LCD_D5_PIN 13
+#define LCD_D6_PORT GPIOA
+#define LCD_D6_PIN 12
+#define LCD_D7_PORT GPIOA
+#define LCD_D7_PIN 11
  
- #define LCD_RS_0 (GPIOA->ODR &= ~BIT15)
- #define LCD_RS_1 (GPIOA->ODR |= BIT15)
- #define LCD_E_0 (GPIOB->ODR &= ~BIT3)
- #define LCD_E_1 (GPIOB->ODR |= BIT3)
- #define LCD_D4_0 (GPIOA->ODR &= ~BIT14)
- #define LCD_D4_1 (GPIOA->ODR |= BIT14)
- #define LCD_D5_0 (GPIOA->ODR &= ~BIT13)
- #define LCD_D5_1 (GPIOA->ODR |= BIT13)
- #define LCD_D6_0 (GPIOA->ODR &= ~BIT12)
- #define LCD_D6_1 (GPIOA->ODR |= BIT12)
- #define LCD_D7_0 (GPIOA->ODR &= ~BIT11)
- #define LCD_D7_1 (GPIOA->ODR |= BIT11)
+ #define LCD_RS_0 (LCD_RS_PORT->ODR &= ~(1 << LCD_RS_PIN))
+ #define LCD_RS_1 (LCD_RS_PORT->ODR |= (1 << LCD_RS_PIN))
+ #define LCD_E_0 (LCD_E_PORT->ODR &= ~(1 << LCD_E_PIN))
+ #define LCD_E_1 (LCD_E_PORT->ODR |= (1 << LCD_E_PIN))
+ #define LCD_D4_0 (LCD_D4_PORT->ODR &= ~(1 << LCD_D4_PIN))
+ #define LCD_D4_1 (LCD_D4_PORT->ODR |= (1 << LCD_D4_PIN))
+ #define LCD_D5_0 (LCD_D5_PORT->ODR &= ~(1 << LCD_D5_PIN))
+ #define LCD_D5_1 (LCD_D5_PORT->ODR |= (1 << LCD_D5_PIN))
+ #define LCD_D6_0 (LCD_D6_PORT->ODR &= ~(1 << LCD_D6_PIN))
+ #define LCD_D6_1 (LCD_D6_PORT->ODR |= (1 << LCD_D6_PIN))
+ #define LCD_D7_0 (LCD_D7_PORT->ODR &= ~(1 << LCD_D7_PIN))
+ #define LCD_D7_1 (LCD_D7_PORT->ODR |= (1 << LCD_D7_PIN))
  #define CHARS_PER_LINE 16
  #define MAXBUFFER 80
  
